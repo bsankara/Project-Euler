1496b086408bcbdd3745a2fb013599aa408cf1d7
balajisankar1 <balajisankar1@gmail.com>
2015-09-08 21:19:07 -0600

    Solved problem 2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 void checkAllSolutions();
 
 int problem1(int);
+int problem2();
 
 int main(int argc, char** argv) {
 	checkAllSolutions();
@@ -16,6 +17,8 @@ int main(int argc, char** argv) {
 void checkAllSolutions() {
 	assert(problem1(1000) == 233168);
 	cout << "Problem 1 was solved correctly" << endl;
+	assert(problem2() == 4613732);
+	cout << "Problem 2 was solved correctly" << endl;
 }
 
 int problem1(int limit) {
@@ -23,6 +26,21 @@ int problem1(int limit) {
 	for (int i = 0; i < limit; i++) {
 		if (i%3 == 0 || i%5 ==0)
 			total+=i;
+	}
+	return total;
+}
+
+int problem2() {
+	int total = 0;
+	int a = 1;
+	int b = 1;
+	int max = 4000000;
+	while(a < max) {
+		int temp = a;
+		a = a + b;
+		b = temp;
+		if (a%2 == 0)
+			total +=a;
 	}
 	return total;
 }
